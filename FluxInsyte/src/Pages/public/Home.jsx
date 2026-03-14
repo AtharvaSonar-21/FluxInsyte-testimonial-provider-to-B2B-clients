172d56cea393b85b4b58ec1df89d7f1a6ba3feec
Atharva Prakash Sonar <atharvasonar654@gmail.com>
2026-03-14 23:26:07 +0530

    Initialize project and add responsive navigation bar

---
+++ FluxInsyte/src/Pages/public/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigation } from "../../Components/common/Navigation";
+export default function Home() {
+    return(
+        <>
+            <nav>
+                <Navigation/>
+            </nav>
+        </>
+    )
+}
